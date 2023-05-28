48cae42295621ffaca70bd96fcdd2356b5adc55f
Sash <sasheng04@gmail.com>
2023-05-28 10:17:28 +0000

    Add structure for multipages for streamlit

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -10,9 +10,12 @@ def hypothesis_body():
 
     st.info(
         "### Project Hypothesis\n\n"
-        "The following are the hypotheses that I have made for this project:\n\n"
-        "1. I assume that a house with a higher OverallQual would sell for a higher price.\n"
+        "The following are the hypotheses that I put forward for this project:\n\n"
+        "1. I assume that a house with greater OverallQual would sell for a higher price.\n"
         "We could possible confirm this with a correlation analysis between OverallQual and SalePrice.\n\n"
         "2. I further assume that a house with a bigger garage sells for a higher price.\n"
         "We could possible confirm this with a correlation analysis between GarageArea and SalePrice can show this relationship.\n\n"
     )
+
+    st.write("---")
+    st.write("### Hypothesis Validation")

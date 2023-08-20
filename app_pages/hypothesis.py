7aaa922b0832cd9b39e19b66637503c4417ca192
Sash <sasheng04@gmail.com>
2023-08-20 09:33:01 +0000

    Add project hypothesis

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -5,17 +5,19 @@ from the Churnometer Walkthrough Project 2.
 
 import streamlit as st
 
-def hypothesis_body():
+def page_project_hypothesis_body():
 
-    st.info(
-        "### Project Hypothesis\n\n"
-        "The following are the hypotheses that I put forward for this project:\n\n"
-        "1. I assume that a house with greater OverallQual would sell for a higher price.\n"
-        "We could possible confirm this with a correlation analysis between OverallQual and SalePrice.\n\n"
-        "2. I further assume that a house with a greater GrLivArea sells for a higher price.\n"
-        "We could possible confirm this with a correlation analysis between GrLivArea and SalePrice can show this relationship.\n\n"
+    st.success(
+        f"### Project Hypothesis and Approval\n\n"
+        f"* The following are the hypotheses that I have made for this project: "
+        f" The need to evaluate other houses in the area of Ames, Iowa, based on similar attributes"
+        f" of the clients 4 inherited houses will give us a"
+        f"prediction of sales price for each house respectively\n\n"
+
+        f"* 1. I assume that the ground floor "
+        f"living area, first floor, basement and the garage, play a "
+        f"key role in determining sale price.\n"
+        f"* 2. The year the house was built and the overall quality "
+        f"of materials used and the finishes in the house also play a "
+        f"significant role in determining house price.\n"
     )
-
-    # Validation of hypotheses
-    st.write("---")
-    st.write("### Hypothesis Validation")

7f70cee57bd813754f97182397e50a513aba85dd
Sash <sasheng04@gmail.com>
2023-08-20 09:00:19 +0000

    Include dataset guideline

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -33,7 +33,35 @@ def page_summary_body():
         f"[INSERT README URL HERE"
         )
     
-    
+    st.info(
+        f"**Dataset Content Guidelines**\n\n"
+        f"|Variable|Meaning|Units|\n"
+        f"|:----|:----|:----|\n"
+        f"|1stFlrSF|First Floor square feet|334 - 4692|\n"
+        f"|2ndFlrSF|Second floor square feet|0 - 2065|\n"
+        f"|BedroomAbvGr|Bedrooms above grade (does NOT include basement bedrooms)|0 - 8|\n"
+        f"|BsmtExposure|Refers to walkout or garden level walls|Gd: Good Exposure; Av: Average Exposure; Mn: Mimimum Exposure; No: No Exposure; None: No Basement|\n"
+        f"|BsmtFinType1|Rating of basement finished area|GLQ: Good Living Quarters; ALQ: Average Living Quarters; BLQ: Below Average Living Quarters; Rec: Average Rec Room; LwQ: Low Quality; Unf: Unfinshed; None: No Basement|\n"
+        f"|BsmtFinSF1|Type 1 finished square feet|0 - 5644|\n"
+        f"|BsmtUnfSF|Unfinished square feet of basement area|0 - 2336|\n"
+        f"|TotalBsmtSF|Total square feet of basement area|0 - 6110|\n"
+        f"|GarageArea|Size of garage in square feet|0 - 1418|\n"
+        f"|GarageFinish|Interior finish of the garage|Fin: Finished; RFn: Rough Finished; Unf: Unfinished; None: No Garage|\n"
+        f"|GarageYrBlt|Year garage was built|1900 - 2010|\n"
+        f"|GrLivArea|Above grade (ground) living area square feet|334 - 5642|\n"
+        f"|KitchenQual|Kitchen quality|Ex: Excellent; Gd: Good; TA: Typical/Average; Fa: Fair; Po: Poor|\n"
+        f"|LotArea| Lot size in square feet|1300 - 215245|\n"
+        f"|LotFrontage| Linear feet of street connected to property|21 - 313|\n"
+        f"|MasVnrArea|Masonry veneer area in square feet|0 - 1600|\n"
+        f"|EnclosedPorch|Enclosed porch area in square feet|0 - 286|\n"
+        f"|OpenPorchSF|Open porch area in square feet|0 - 547|\n"
+        f"|OverallCond|Rates the overall condition of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|\n"
+        f"|OverallQual|Rates the overall material and finish of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|\n"
+        f"|WoodDeckSF|Wood deck area in square feet|0 - 736|\n"
+        f"|YearBuilt|Original construction date|1872 - 2010|\n"
+        f"|YearRemodAdd|Remodel date (same as construction date if no remodeling or additions)|1950 - 2010|\n"
+        f"|SalePrice|Sale Price|34900 - 755000|\n"
+        )    
 
       
 

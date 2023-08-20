ec86cd9bf75b8b4fd422156cd8d1cbae6142a301
Sash <sasheng04@gmail.com>
2023-08-20 12:32:38 +0000

    Generate paths for sale price pipelines

--- app_pages/price_predictor.py
+++ app_pages/price_predictor.py
@@ -3,23 +3,24 @@ This file and its contents have been informed and adapted
 from the Churnometer Walkthrough Project 2.
 '''
 
-# load predict sale price files
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.file_management import load_clean_data, load_pkl_file
+from src.eval_pipeline_perf import regression_performance
+from src.eval_pipeline_perf import regression_evaluation_plots
 
-# Generate Live Data
+def page_predict_sale_price_body():
 
-# predict on live data
+    # load sale price pipeline files
+    ver = 'v2'
+    path = f"outputs/ml_pipeline/predict_saleprice/{ver}"
+    sale_price_pipe = load_pkl_file(f"{path}/best_regressor_pipeline.pkl")
+    feat_importance = pd.read_csv(f"{path}/feature_importance.csv")
+    feat_importance_plot = plt.imread(f"{path}/feature_importance.png")
+    X_train = pd.read_csv(f"{path}/X_train.csv")
+    X_test = pd.read_csv(f"{path}/X_test.csv")
+    y_train = pd.read_csv(f"{path}/y_train.csv")
+    y_test = pd.read_csv(f"{path}/y_test.csv")
 
-# logic to display the sale price
-
-# load dataset
-
-# we create input widgets for all features
-
-# We are using these features to feed the ML pipeline
-# - values copied from check_variables_for_UI() result
-# 'OverallQual', 'GrLivArea', 'GarageArea', 'YearBuilt'
-
-# create an empty DataFrame, which will be the live data
-
-# from here on we draw the widget based on the variable
-# type (numerical or categorical) and set initial values
+    st.write("### ML Pipeline: Predict House Sale Price")

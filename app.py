f76b1f112b90b6622ebf4d8367f373d89a25c356
Sash <sasheng04@gmail.com>
2023-08-20 16:37:36 +0000

    Adjust app page naming

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-from app_pages.multi_page import MultiPage
+from app_pages.page_multi_page import MultiPage
 
 # load pages scripts
-from app_pages.summary import page_summary_body
-from app_pages.predict_sale_price import page_sale_price_study_body
-from app_pages.price_predictor import page_price_predictor_body
-from app_pages.hypothesis import page_project_hypothesis_body
-from app_pages.predict_sale_price import page_predict_sale_price_body
+from app_pages.page_summary import page_summary_body
+from app_pages.page_predict_sale_price import page_sale_price_study_body
+from app_pages.page_price_predictor import page_price_predictor_body
+from app_pages.page_hypothesis import page_project_hypothesis_body
+from app_pages.page_predict_sale_price import page_predict_sale_price_body
 
 # Create an instance of the app
 app = MultiPage(app_name="House Price Predictor")

f0ab2c1676cf50b2e193bcd76809d2bf2385df5c
Sash <sasheng04@gmail.com>
2023-08-30 09:11:18 +0000

    Add validation of hypotheses

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -21,3 +21,17 @@ def page_hypothesis():
         f"of materials used (finishes) in the house also play a "
         f"significant role in determining houses sales price.\n"
     )
+
+    st.write("---")
+    st.write("### Hypothesis Validation")
+    st.write("The following are the results of the hypothesis validation:\n\n"
+            "1. The correlation analysis between GrLivArea and SalePrice shows a strong positive correlation.\n"
+            "* This means that houses with a greater GrLivArea results in a higer SalePrice.\n\n"
+            "* We can see this from the scatter plot below, the relationship is linear.\n\n")
+    st.image("images/GrLivArea.png")
+
+    st.write("\n\n2. The correlation analysis between YearBuiltDate and OverallQual towards SalePrice shows a weak link.\n")
+    st.write("* There is a weak link between these qualities and not enough evidence to support this.\n\n")
+    st.write("* We can see this from the scatter plots below.\n\n")
+    st.image("images/OverallQual.png")
+    st.image("images/YearBuilt.png")

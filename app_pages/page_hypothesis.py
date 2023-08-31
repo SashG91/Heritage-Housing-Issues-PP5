bb218adef6c78bd2dce764c754d7600160996289
Sash <sasheng04@gmail.com>
2023-08-31 06:40:07 +0000

    Adjust text for hypthesis

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -10,16 +10,16 @@ def page_hypothesis():
     st.success(
         f"### Project Hypothesis and Approval\n\n"
         f"* The following are the hypotheses that I have made for this project: "
-        f" The need to evaluate other houses in the area of Ames, Iowa, based on similar attributes"
-        f" of the clients 4 inherited houses will give us a"
-        f"prediction of sales price for each house respectively\n\n"
+        f" The need to evaluate other houses in the same area of Ames, Iowa, targetting similar attributes"
+        f" of the clients inherited houses will give us a"
+        f" good prediction of potential sales price of each house. \n\n"
 
         f"* 1. I assume that the ground floor "
         f"living area, first floor, basement and the garage, play a "
         f"key role in determining the sale price.\n"
         f"* 2. The year the house was built and the overall quality "
-        f"of materials used (finishes) in the house also play a "
-        f"significant role in determining houses sales price.\n"
+        f"of materials (finishes used) in the house also play a "
+        f"significant role in determining the house sales price.\n"
     )
 
     st.write("---")
